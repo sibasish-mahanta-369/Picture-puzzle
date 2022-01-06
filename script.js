c243d73931161c31f4599f927b3e2d653637efe0
sibasish-mahanta-369 <91348004+sibasish-mahanta-369@users.noreply.github.com>
2022-01-06 13:02:15 +0530

    added ctl

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function click1() {
     r2.add(className1);
 }
 function click2() {
-    var className1 = document.getElementById("d4").className;
+    var className1 = document.getElementById("d3").className;
     var className2 = document.getElementById("d2").className;
-    r = document.getElementById("d4").classList;
+    r = document.getElementById("d3").classList;
     r.remove(className1);
     r.add(className2);
     r2 = document.getElementById("d2").classList;
@@ -21,11 +21,11 @@ function click2() {
 }
 function click4() {
     var className1 = document.getElementById("d1").className;
-    var className2 = document.getElementById("d3").className;
+    var className2 = document.getElementById("d4").className;
     r = document.getElementById("d1").classList;
     r.remove(className1);
     r.add(className2);
-    r2 = document.getElementById("d3").classList;
+    r2 = document.getElementById("d4").classList;
     r2.remove(className2);
     r2.add(className1);
 }
@@ -38,4 +38,33 @@ function click3() {
     r2 = document.getElementById("d4").classList;
     r2.remove(className2);
     r2.add(className1);
+}
+function reset(){
+    alert("Puzzle will be reset");
+    var className1 = document.getElementById("d1").className;
+    var className2 = document.getElementById("d2").className;
+    var className3 = document.getElementById("d3").className;
+    var className4 = document.getElementById("d4").className;
+    r = document.getElementById("d1").classList;
+    r2 = document.getElementById("d2").classList;
+    r3 = document.getElementById("d3").classList;
+    r4 = document.getElementById("d4").classList;
+    r.remove(className1);
+    r2.remove(className2);
+    r3.remove(className3);
+    r4.remove(className4);
+}
+function reseta(){
+    d1.classList.add("i1");
+    d2.classList.add("i2");
+    d3.classList.add("i3");
+    d4.classList.add("i4");
+}
+function submit(){
+    if (document.getElementById("d1").className == "i2" && document.getElementById("d2").className == "i4" && document.getElementById("d3").className == "i3" && document.getElementById("d4").className == "i1") {
+        alert("Congratulations you have sucessfully solved the puzzle!! :)");
+    }
+    else{
+        alert("The puzzle is not solved yet :(");
+    }
 }
